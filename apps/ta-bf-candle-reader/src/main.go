77965fae0b46d9a81e7859751a46a24969d4e40f
David Borrego <dborrego@redhat.com>
2022-04-08 08:17:29 +0200

    clean

--- apps/ta-bf-candle-reader/src/main.go
+++ apps/ta-bf-candle-reader/src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -11,7 +10,6 @@ import (
 	"ta-bf-candle-reader/adapter"
 	"ta-bf-candle-reader/interfaces"
 
-	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/candle"
 	"github.com/bitfinexcom/bitfinex-api-go/pkg/models/common"
 	"github.com/bitfinexcom/bitfinex-api-go/v1"
 	"github.com/gin-gonic/gin"
@@ -48,34 +46,9 @@ func main() {
 		ws.Subscribe(common.TradingPrefix+bitfinex.BTCUSD, common.OneDay)
 	}()
 
-	// client := websocket.New()
-	// err := client.Connect()
-	// if err != nil {
-	// 	log.Printf("could not connect: %s", err.Error())
-	// 	return
-	// }
-	// go func() {
-	// 	for msg := range client.Listen() {
-	// 		t, candle := msg.(*candle.Candle)
-	// 		if candle {
-	// 			log.Printf("%s", toJson(t))
-	// 		}
-	// 		if _, ok := msg.(*websocket.InfoEvent); ok {
-	// 			_, err := client.SubscribeCandles(ctx, common.TradingPrefix+bitfinex.BTCUSD, common.FiveMinutes)
-	// 			if err != nil {
-	// 				log.Printf("could not subscribe to candles: %s", err.Error())
-	// 			}
-	// 		}
-	// 	}
-	// }()
 	<-ctx.Done()
 	ws.Close()
 	srv.Shutdown(ctx)
 	os.Exit(0)
 	log.Println("Cerrando")
-}
-
-func toJson(c *candle.Candle) string {
-	body, _ := json.Marshal(c)
-	return string(body)
 }
