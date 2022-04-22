cd2c1355efc47781e506be947826c1878b16a058
David Borrego <dborrego@redhat.com>
2022-04-22 10:02:55 +0200

    candle model

--- apps/ta-candle-store/src/domain/model/Candle.go
+++ apps/ta-candle-store/src/domain/model/Candle.go
@@ -1,6 +1,13 @@
 package model
 
 type Candle struct {
-	name string
-	size int
+	symbol    string
+	market    string
+	precision string
+	ts        int32
+	open      float32
+	close     float32
+	high      float32
+	low       float32
+	volume    float32
 }
